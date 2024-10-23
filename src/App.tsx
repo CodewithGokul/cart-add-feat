38f063b84ee7ca1831b14fd959d502b9dee81152
CodewithGokul <gokulvijayuph@gmail.com>
2024-10-23 14:49:34 +0530

    v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import data from "./data";
 import "./App.css";
 
@@ -8,92 +8,115 @@ type cartlist = {
   price: number;
 };
 
-
 function App() {
   const [cartEnabled, setCartEnabled] = useState(new Array(9).fill(false));
   const [cartValue, setCartValue] = useState(new Array(9).fill(0));
   const [cartList, setCartList] = useState<cartlist[]>([]);
+  const [total,setTotal] = useState(0);
 
-  const cartEnabledHandler = (i: number) => {
-    setCartEnabled(
-      cartEnabled.map((bool, index) => (index === i ? true : bool))
-    );
+
+  useEffect(() => {
+    const updatedCartEnabled = cartValue.map((val) => val > 0);
+    setCartEnabled(updatedCartEnabled);
+  }, [cartValue]);
+
+  
+  const Decrement = (name: string, i: number) => {
+    // Find index of the item in the cart list
+    const itemIndex = cartList.findIndex((val) => val.name === name);
+
+    if (itemIndex > -1) {
+      // Decrease the count for the corresponding item
+      const newCartValue = cartValue.map((val, index) => (index === i ? val - 1 : val));
+      setCartValue(newCartValue);
+        const updatedCartList = cartList.filter((_, index) => index !== itemIndex);
+        setCartList(updatedCartList);
+    }
   };
 
-  const Decrement = (i: number) => {
-
-    setCartValue(cartValue.map((val, index) => (index === i ? val - 1 : val)));
-
-    setCartList(cartList.filter((_,index)=>(index!==i)))
-
-    console.log(cartList);
-  };
- 
   const Increment = (i: number) => {
-
     const obj: cartlist = {
       category: data[i].category,
       name: data[i].name,
       price: data[i].price,
     };
-    setCartList((curntVal) =>([...curntVal, obj]));
-    setCartValue(cartValue.map((val, index) => (index === i ? val + 1 : val)));
+
+    setCartList((currentVal) => [...currentVal, obj]);
+
+    setCartValue((currentCartValue) =>
+      currentCartValue.map((val, ind) => (ind === i ? val + 1 : val))
+    );
   };
+
   return (
     <>
       <div className="container">
         <div className="cart-container">
           <h1>Desserts</h1>
           {data.map((_, i: number) => (
-            <div className="box">
+            <div className="box" key={i}>
               <div className="img">
-                <img className="image-cart" src={data[i].image.desktop} />
+                <img
+                  className="image-cart"
+                  src={data[i].image.desktop}
+                  alt={data[i].name}
+                />
               </div>
 
               {!cartEnabled[i] ? (
                 <button
                   className="cart-btn"
                   onClick={() => {
-                    cartEnabledHandler(i);
                     Increment(i);
                   }}
                 >
                   Add to Cart
                 </button>
               ) : (
-                cartEnabled[i] && (
-                  <button className="btn-added">
-                    <div className="minus btn-action" onClick={() => Decrement(i)}>
-                      -
-                    </div>
-                    <div className="count">{cartValue[i]}</div>
-                    <div className="plus btn-action" onClick={() => Increment(i)}>
-                      +
-                    </div>
-                  </button>
-                )
+                <button className="btn-added">
+                  <div
+                    className="minus btn-action"
+                    onClick={() => {
+                      Decrement(data[i].name, i);
+                    }}
+                  >
+                    -
+                  </div>
+                  <div className="count">{cartValue[i]}</div>
+                  <div className="plus btn-action" onClick={() => Increment(i)}>
+                    +
+                  </div>
+                </button>
               )}
-                <div className="food-category">{data[i].category}</div>
-                <div className="food-name">{data[i].name}</div>
-                <div className="price">{data[i].price}</div>
-              </div>
+              <div className="food-category">{data[i].category}</div>
+              <div className="food-name">{data[i].name}</div>
+              <div className="price">{data[i].price}</div>
+            </div>
           ))}
         </div>
 
         <div className="your-cart">
           {cartList.length === 0 ? (
             <>
-              <img src="src/assets/illustration-empty-cart.svg" />
+              <img
+                src="src/assets/illustration-empty-cart.svg"
+                alt="Empty Cart"
+              />
               <h4>Your Items Will Be Added here!</h4>
             </>
           ) : (
-            cartList.map((val: cartlist,_) => (
-              <>
-                  <div className="cart-category cart-list-child">{val.category}</div>
+            <>
+            <div className="cart-no">Your Cart ({cartList.length})</div>
+              {cartList.map((val: cartlist, idx: number) => (
+                <div key={idx}>
+                  <div className="cart-category cart-list-child">
+                    {val.category}
+                  </div>
                   <div className="cart-name cart-list-child">{val.name}</div>
                   <div className="cart-price cart-list-child">{val.price}</div>
-              </>
-            ))
+                </div>
+              ))}
+            </>
           )}
         </div>
       </div>
